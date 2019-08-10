d55d1c5db33fe55c2e3484f9967f85f9a0d215b3
Alfi <alfi.potter@d3r.com>
2019-08-10 08:31:36 +0100

    build

--- .next/static/runtime/webpack.js
+++ .next/static/runtime/webpack.js
@@ -113,7 +113,7 @@
 /******/
 /******/ 	var hotApplyOnUpdate = true;
 /******/ 	// eslint-disable-next-line no-unused-vars
-/******/ 	var hotCurrentHash = "998d4c8f88df0589738c";
+/******/ 	var hotCurrentHash = "0aa33c792c960dbd318d";
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule;
--- next.config.js
+++ next.config.js
@@ -1,7 +1,6 @@
 // next.config.js
 const withSass = require('@zeit/next-sass')
 const withCss = require('@zeit/next-css');
+const withImages = require('next-optimized-images');
 
-module.exports = withSass(withCss({
-  /* config options here */
-}));
+module.exports = withSass(withCss(withImages({optimizeImagesInDev: true})));
